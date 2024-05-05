bbc7049b6a00bba0fbd404c9c686778336377ac8
serdy <serdyukovantoniy@gmail.com>
2024-05-05 22:59:11 +0300

    Second function: calculates top files a contributor has contributed

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import time
 from git import Repo
 import os
 
-remoteurl = "https://github.com/an-tonic/CO1111-Shared-Project.git"
+remoteurl = "https://github.com/an-tonic/test-repo-for-jetbrains.git"
+
 folder_path = "D:/tmp"
 
 if os.path.exists(folder_path):
@@ -32,3 +33,27 @@ def calculate_top_contributors(repository):
 
 
 print(calculate_top_contributors(repo))
+
+def display_contributions_by_author(repository, author_email):
+    contributions = {}
+
+    # Traverse commits
+    for commit in repository.iter_commits(reverse=True):
+        if commit.author.email == author_email:
+            # skipping the first because has no parents
+            if len(commit.parents) == 0:
+                continue
+            modified_files = [item.a_path for item in commit.parents[0].diff(commit)]
+
+            for file_path in modified_files:
+                if file_path in contributions:
+                    contributions[file_path] += 1
+                else:
+                    contributions[file_path] = 1
+
+    sorted_contributors = dict(sorted(contributions.items(), key=lambda item: item[1], reverse=True))
+
+    return sorted_contributors
+
+
+print(display_contributions_by_author(repo, "96774237+an-tonic@users.noreply.github.com"))
