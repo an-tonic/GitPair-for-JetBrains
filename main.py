9e16e4adfc34f3c845db5f4b01679402fa2b951a
serdy <serdyukovantoniy@gmail.com>
2024-05-05 22:44:20 +0300

    First function: calculates top contributors in a repo

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import time
 from git import Repo
 import os
 
-remoteurl = "https://github.com/an-tonic/test-repo-for-jetbrains.git"
-
-
+remoteurl = "https://github.com/an-tonic/CO1111-Shared-Project.git"
 folder_path = "D:/tmp"
+
 if os.path.exists(folder_path):
     repo = Repo(folder_path)
     # origin = repo.remotes.origin
@@ -14,17 +13,22 @@ if os.path.exists(folder_path):
 else:
     repo = Repo.clone_from(remoteurl, folder_path)
 
-for commit in repo.iter_commits():
-    print(commit.diff())
-    # Extract the list of files/modules modified in the commit
-    # modified_files = [item.a_path for item in commit.diff()]
-    # for item in commit.diff():
-    #     print(item.a_path)
-    # # Record contributors for each modified file/module
-    # for file_path in modified_files:
-    #     contributors_per_file[file_path].add(commit.author.email)
+
+def calculate_top_contributors(repository):
+    contributors = {}
+
+    for commit in repository.iter_commits(reverse=True):
+        author_email = commit.author.email
+
+        if author_email in contributors:
+            contributors[author_email] += 1
+        else:
+            contributors[author_email] = 1
+
+    # Sort contributors by number of commits
+    sorted_contributors = dict(sorted(contributors.items(), key=lambda item: item[1], reverse=True))
+
+    return sorted_contributors
 
 
-
-
-
+print(calculate_top_contributors(repo))
